c2ff68d9058eec0dd248cc7519715f4d020148e4
pavanigarlapati <pavanigarlapati05@gmail.com>
2026-01-03 20:59:38 +0530

    Create App.js

---
+++ App.js
@@ -0,0 +1,48 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./components/Login";
+import AdminDashboard from "./pages/AdminDashboard";
+import CustomerDashboard from "./pages/CustomerDashboard";
+import UpdateRestaurant from "./pages/UpdateRestaurant";
+import ProtectedRoute from "./components/ProtectedRoute";
+import { AuthProvider } from "./context/AuthContext";
+
+function App() {
+  return (
+    <AuthProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Login />} />
+
+          <Route
+            path="/admin/dashboard"
+            element={
+              <ProtectedRoute role="admin">
+                <AdminDashboard />
+              </ProtectedRoute>
+            }
+          />
+
+          <Route
+            path="/admin/restaurants/update"
+            element={
+              <ProtectedRoute role="admin">
+                <UpdateRestaurant />
+              </ProtectedRoute>
+            }
+          />
+
+          <Route
+            path="/customers/dashboard"
+            element={
+              <ProtectedRoute role="customer">
+                <CustomerDashboard />
+              </ProtectedRoute>
+            }
+          />
+        </Routes>
+      </BrowserRouter>
+    </AuthProvider>
+  );
+}
+
+export default App;
